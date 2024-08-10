1136f182172b2ba7040f0ff854d50e4187edb2bb
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-08-10 18:00:43 +0200

    feat: add outline support for properties, constants, and enums
    Part of #12

--- languages/gdscript/outline.scm
+++ languages/gdscript/outline.scm
@@ -5,3 +5,27 @@
 (function_definition
     "func" @context
     name: (_) @name) @item
+
+(const_statement
+    "const" @context
+    name: (_) @name) @item
+
+; Annotated variables use sibling nodes to store the annotation
+; TODO: find a way to capture the annotation in the outline to distinguish
+; annotated variables from regular variables
+(_
+  (annotation) @context
+  (variable_statement
+    "var" @context
+    name: (_) @name
+  )
+) @item
+
+(variable_statement
+    "var" @context
+    name: (_) @name
+) @item
+
+(enum_definition
+    "enum" @context
+    name: (_) @name) @item
