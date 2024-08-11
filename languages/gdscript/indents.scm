39247f14b9f8a826f59df138b957382cde517ff4
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-08-11 22:48:01 +0200

    chore: revert "fix for indentation"
    This reverts commit cf472ab24182a442e276cb69814606ff73f6149c.
    
    close #21
    close #20

--- languages/gdscript/indents.scm
+++ languages/gdscript/indents.scm
@@ -1,3 +1,34 @@
-(_ "[" "]" @end) @indent
-(_ "{" "}" @end) @indent
-(_ "(" ")" @end) @indent
+[
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (match_statement)
+  (pattern_section)
+
+  (function_definition)
+  (constructor_definition)
+  (class_definition)
+  (enum_definition)
+
+  (dictionary (_))
+  (array (_))
+  (setget)
+] @indent
+
+[
+  (if_statement)
+  (for_statement)
+  (while_statement)
+  (match_statement)
+  (pattern_section)
+
+  (function_definition)
+  (class_definition)
+] @extend
+
+[
+  (return_statement)
+  (break_statement)
+  (continue_statement)
+  (pass_statement)
+] @extend.prevent-once
