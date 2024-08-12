18227c1cef1118bcf593c95ff94d3a0c72d7b1c2
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2024-08-12 12:54:05 +0200

    fix: fix indentation being lost after code blocks and line losing indentation when inserting a line above
    fix #10

--- languages/gdscript/indents.scm
+++ languages/gdscript/indents.scm
@@ -1,38 +1,3 @@
-[
-  (if_statement)
-  (for_statement)
-  (while_statement)
-  (match_statement)
-  (pattern_section)
-
-  (function_definition)
-  (constructor_definition)
-  (class_definition)
-  (enum_definition)
-
-  (dictionary (_))
-  (array (_))
-  (setget)
-] @indent
-
-[
-  (if_statement)
-  (for_statement)
-  (while_statement)
-  (match_statement)
-  (pattern_section)
-
-  (function_definition)
-  (class_definition)
-] @extend
-
-[
-  (return_statement)
-  (break_statement)
-  (continue_statement)
-  (pass_statement)
-] @extend.prevent-once
-
 ; Add an indent level when adding a line within brackets
 (_ "[" "]" @end) @indent
 (_ "{" "}" @end) @indent
