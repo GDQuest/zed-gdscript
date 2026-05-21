8943bbc7df534043c6bf06c3f284153697d01201
Yuri Sizov <11782833+YuriSizov@users.noreply.github.com>
2026-05-21 12:25:10 +0200

    Highlight setget methods and punctuation in GDScript (#90)

--- data/parsing_test.gd
+++ data/parsing_test.gd
@@ -2,6 +2,17 @@
 class_name MyClass extends Sprite2D
 
 var property := Vector2(480, -480)
+var array_property: Array[Vector2] = []
+
+@export var exported_property: int = 42:
+	get:
+		return 4
+	set(value):
+		exported_property = value
+var setget_property: float = 3.2:
+	get = get_setget_property,
+	set = set_setget_property
+
 
 func _init() -> void:
 	pass
